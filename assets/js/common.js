5861925fd5bfb1c1b6a1b1c4e3ad43c523c637cd
FrontLAB <123484134+Frontlab-kr@users.noreply.github.com>
2024-07-08 09:54:31 +0900

    레시피 관리 시스템

---
+++ assets/js/common.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $('#header').load('./components/header.html');
+});
+
+//snb
+var statusSnb = 'unfold';
+function snbToggle() {
+  if (statusSnb === 'unfold') {
+    $('.isu').addClass('snb-fold');
+    statusSnb = 'fold';
+    $('.isu-snb .collapse').removeClass('show');
+  } else {
+    $('.isu').removeClass('snb-fold');
+    statusSnb = 'unfold';
+  }
+}
+
+$(document).on('click', '.isu-snb-item .btn', function (e) {
+  if (statusSnb === 'fold') {
+    $('.isu').removeClass('snb-fold');
+    statusSnb = 'unfold';
+  }
+});
+
+$(document).ready(function () {
+  //flatpickr
+  $('.flatpickr-input').flatpickr({
+    dateFormat: 'Y-m-d',
+    locale: 'ko',
+  });
+  $('.flatpickr-inline').flatpickr({
+    dateFormat: 'Y-m-d',
+    locale: 'ko',
+    inline: true,
+  });
+});
